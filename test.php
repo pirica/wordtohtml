16c984d424d8bd557d3d47f762fe7a496fcc22f5
Timothy Edwards <91966621+timy352@users.noreply.github.com>
2023-09-09 09:05:46 +0100

    Update test.php
    Update for version v2.1.11

--- test.php
+++ test.php
@@ -1,6 +1,9 @@
 <?php
 require_once('wordphp.php');
 $rt = new WordPHP(false);
-$text = $rt->readDocument('sample.docx','NN');
+$text = $rt->readDocument('sample.docx','N');
 echo $text;
+// The following two lines are optional if required to save the html text to a file (newfile.php)
+$myfile = fopen("newfile.php", "w") or die("Unable to open file!");
+fwrite($myfile, $text);
 ?>
